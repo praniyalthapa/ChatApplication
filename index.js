3fab4784bb87f0a1126b17aaeb3b8851cb91eecd
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-03-18 17:24:07 +0545

    added views folder and installed ejs view-engine

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const io=socketio(server);
 io.on('connection', (socket) => {
     console.log('a user connected',socket.id);
   //socket.on is for liestening the events from frontend
-  socket.on('msg-send',(data)=>{ //when u r listening to the event you need a callback which takes parameter (data) in which there is same object which u r sending
-    console.log(data); //listening the message from server 
-    // io.emit('msg-received',data);//server is going to now emit/semd and event all the connection that are present 
-    //  socket.emit('msg-received',data);//it will not send data too all the connected services
-    io.emit('msg-received',data); //except the sender other will get the message
-  });
+  // socket.on('msg-send',(data)=>{ //when u r listening to the event you need a callback which takes parameter (data) in which there is same object which u r sending
+  //   console.log(data); //listening the message from server 
+  //   // io.emit('msg-received',data);//server is going to now emit/semd and event all the connection that are present 
+  //   //  socket.emit('msg-received',data);//it will not send data too all the connected services
+  //   io.emit('msg-received',data); //except the sender other will get the message
+  // });
+
    
    
 
@@ -23,9 +24,15 @@ io.on('connection', (socket) => {
 
   });
 //in express to connect with static file like html we do
+app.set('view engine','ejs');
 app.use('/',express.static(__dirname + '/public')); //this middleware maps where your static file is located
 
-
+app.get('/chat/:roomid',(req,res)=>{
+  res.render('index',{
+    name:'Praniyal',
+    id:req.params.roomid
+  });
+})
 
 server.listen(3000,async()=>{
     console.log("Server started");
